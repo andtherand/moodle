96833baddfd110972b30b95e2926a7a1eeaf3c0d
Andy Ruck <mychiara@gmail.com>
2014-10-16 15:19:27 +0200

    added missing defaults and fixed a typo

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,14 +2,22 @@ default[:moodle][:repository] = "git://git.moodle.org/moodle.git"
 default[:moodle][:branch] = "master"
 default[:moodle][:https] = false
 
-default[:moodle][:dbtype] = "mysqli"
-default[:moodle][:dblibrary] = "native"
 default[:moodle][:host] = "localhost"
+
+# db stuff
 default[:moodle][:prefix] = "mdl_"
+
+default[:moodle][:dbtype] = "mysqli"
 default[:moodle][:dbpersist] = false
+default[:moodle][:dblibrary] = "native"
 default[:moodle][:dbsocket] = false
 default[:moodle][:dbport] = ""
-default[:moodle][:directorypermission] = 02777
+default[:moodle][:dbuser] = 'mdl_db_user'
+default[:moodle][:dbpass] = 'I_really_should_be_changed,man!'
+default[:moodle][:dbname] = 'moodle'
+default[:moodle][:passwordsaltmain] = 'Er-zxpsd<$lwl1&el-$*C~`Sv<NE4z:h9sL&pJ!0+7HfZtSbp(mCdKBk:vb|309.'
+
+default[:moodle][:directorypermissions] = 02777
 default[:moodle][:admin] = 'admin'
 
 default[:moodle][:project][:log_level] = 'warn'
