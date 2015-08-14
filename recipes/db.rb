5f3b4a5c7a59578d725b08795fb05fdfae395cc0
Andy Ruck <mychiara@gmail.com>
2015-08-14 15:31:40 +0200

    added socket to connection

--- recipes/db.rb
+++ recipes/db.rb
@@ -3,7 +3,8 @@ db = node[:moodle]
 mysql_connection_info = {
     :host => node[:moodle][:host],
     :username => 'root',
-    :password => node[:mysql][:server_root_password]
+    :password => node[:mysql][:server_root_password],
+    :socket => '/var/run/mysql-default/mysqld.sock'
 }
 
 # create the database
